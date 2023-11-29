e9588addbceb148b6d3a044f396679a031249d89
Khánh Đinh <101515169+KhanhDinh03@users.noreply.github.com>
2023-11-30 01:41:14 +0700

    sửa connection

--- admin/connection.php
+++ admin/connection.php
@@ -1,1 +1,16 @@
+<?php
+function getCon() {
+    $db = 'BTTH01_CSE485';
+    $user = 'root';
+    $pass = '1';
+    $charset = 'utf8mb4';
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    try {
+        $conn = new PDO($dsn, $user, $pass);
+        return $conn;
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+        return null;
+    }
+}
 
