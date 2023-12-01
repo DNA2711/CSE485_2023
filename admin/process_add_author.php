579cc4109045477812f0d644a521fee1d2049705
Khánh Đinh <101515169+KhanhDinh03@users.noreply.github.com>
2023-12-01 11:38:34 +0700

    chỉnh sửa 1 chút

--- admin/process_add_author.php
+++ admin/process_add_author.php
@@ -1,20 +1,19 @@
 <?php
-// Kiểm tra khi form được submit
+include ('connection.php')
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['txtAuthorName'])) {
-    $db_conn = mysqli_connect("localhost","root","1","BTTH01_CSE485");
+    $conn = getCon();
 
     $ten_tgia = $_POST['txtAuthorName'];
 
     // Chuẩn bị truy vấn thêm dữ liệu
-    $sql = "INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES ('13','$ten_tgia')";
+    $sql = "INSERT INTO tacgia (ten_tgia) VALUES ('$ten_tgia')";
 
-    $insert_data = $db_conn->query($sql);
+    $stmt = $conn->prepare($sql);
 
-
-  /*  if ($db_conn->query($sql) === TRUE) {
-        echo "Thêm Thể loại thành công";
+    if ($stmt->execute()) {
+        echo "Thêm dữ liệu thành công.;
     } else {
-        echo "Lỗi: " . $sql . "<br>" . localhost->error;
-    }*/
+        echo "Thêm dữ liệu thất bại.;
+    }
 }
 ?>
